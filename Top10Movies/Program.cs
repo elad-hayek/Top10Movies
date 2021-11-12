6ddd7eea7faa4342996e6431108e6a9624b073df
elad3283@gmail.com <zxcvvcxz!123>
2021-11-12 19:21:09 +0200

    added cors

--- Top10Movies/Program.cs
+++ Top10Movies/Program.cs
@@ -1,5 +1,7 @@
 using Top10Movies.Services;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IDbHandlerService, DbHandlerService>();
 builder.Services.AddScoped<IMoviesService, MoviesService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000");
+                                              
+                      });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
